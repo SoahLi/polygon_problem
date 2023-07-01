2051e0be67e96669c343e00ff4cabd510da116e5
SoahLi <ows8turnbull@gmail.com>
2023-07-01 13:04:56 -0400

    Creating events for improved workflow

---
+++ event_action.py
@@ -0,0 +1,13 @@
+import subprocess
+import vscode
+
+
+command = "sudo killall Python"
+process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+output, error = process.communicate()
+
+if process.returncode == 0:
+    print("Command executed successfully.")
+    print("Output:", output.decode())
+else:
+    print("Error:", error.decode())
