c8cb39ed5238143fa5f3b9839f485f97c81019f8
SoahLi <ows8turnbull@gmail.com>
2023-07-07 15:06:43 -0400

    update event_action

--- event_action.py
+++ event_action.py
@@ -1,12 +1,14 @@
 import subprocess
+
 def killall():
-    print("hello")
     command = "killall Python"
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
-    if process.returncode == 0:
-        print("Command executed successfully.")
-        print("Output:", output.decode())
-    else:
-        print("Error:", error.decode())
-killall()
+    try:
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = process.communicate()
+        if process.returncode == 0:
+            print("Command executed successfully.")
+            print("Output:", output.decode())
+        else:
+            print("Error:", error.decode())
+    except Exception:
+        pass
