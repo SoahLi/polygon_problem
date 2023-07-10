e035d78f72452552c88df9bb1db752505171360e
SoahLi <ows8turnbull@gmail.com>
2023-07-10 13:18:39 -0400

    create border

--- map.py
+++ map.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 from shapely.geometry import Polygon
 import geopandas as gpd
+import geopandas.geoseries as gs
 import pandas as pd
+
 class Map:
     cardinal_directions = {
         "n": ["", "+"],
@@ -18,12 +20,12 @@ class Map:
         self.directions = directions
         self.num_lines = len(line_lengths)
         self.map_coordinates = []
-        self.length, self.width = 0, 0
-        self.border = []
+        self.width, self.height = 0, 0
+        self.border_coordinates = []
         if line_lengths and directions:
             self.map = self.add_polygon(self.create_map_coordinates())
-            self.length, self.width = self.find_area()
-            self.border = self.create_border()
+            self.width, self.height = self.find_area()
+            self.border_coordinates = self.create_border()
         
 
     def create_map_coordinates(self):
@@ -40,21 +42,34 @@ class Map:
                 x_coordinates.append(x_coordinates[-1] + 0)
                 y_coordinates.append(y_coordinates[-1] - self.line_lengths[i])
                 if y_coordinates[-1] < 0:
-                    y_coordinates = self.shift_graph_to_positive(y_coordinates, -y_coordinates[-1])
+                    y_coordinates = self.shift_graph(y_coordinates, -y_coordinates[-1])
             elif self.directions[i] == "w":
                 x_coordinates.append(x_coordinates[-1] - self.line_lengths[i])
                 y_coordinates.append(y_coordinates[-1] + 0)
                 if x_coordinates[-1] < 0:
-                    x_coordinates = self.shift_graph_to_positive(x_coordinates, -x_coordinates[-1])
+                    x_coordinates = self.shift_graph(x_coordinates, -x_coordinates[-1])
+        x_coordinates, y_coordinates = self.shift_graph(x_coordinates, 2), self.shift_graph(y_coordinates, 2)
         for i in range(self.num_lines):
             self.map_coordinates.append((x_coordinates[i], y_coordinates[i]))
-        self.find_area()
+        
         return self.map_coordinates
     
     def create_border(self):
-        pass
-    
-    def shift_graph_to_positive(self, coordinates, length):
+        """
+        Creates border for Map Object
+        """
+        self.border_coordinates = Polygon([(0,0),(self.width,0), (self.width, self.height), (0, self.height)])
+        self.border_coordinates = self.border_coordinates.symmetric_difference(self.polygons[0])
+        self.polygons.append(self.border_coordinates)
+        return self.border_coordinates
+
+    def shift_graph(self, coordinates, length):
+        """
+        Shifts graph to positive coordinates
+
+        Args:
+            coordinates (arr): Coordinates for Map Object
+        """
         for i in range(len(coordinates)):
             coordinates[i] += length
         return coordinates
@@ -69,7 +84,8 @@ class Map:
                 largest_x = current_x
             if current_y > largest_y:
                 largest_y = current_y
-        return largest_x, largest_y
+        print(largest_x,largest_y)
+        return largest_x+2, largest_y+2
 
 
     def add_polygon(self, coordinates=[]):
